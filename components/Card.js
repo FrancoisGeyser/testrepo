9c97ad354b1c524f5865e206943b7932479eb1b8
francoisgeyser <fegeyser@gmail.com>
2021-02-04 13:05:28 +0300

    styled components

---
+++ components/Card.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const CardHolder = styled.div`
+padding: 2rem;
+border: 1px solid gray;
+display:flex;
+justify-content:flex-start;
+`;
+
+const Card = ({data}) => {
+    return (
+        <CardHolder>
+
+        </CardHolder>
+    )
+}
+
+export default Card
